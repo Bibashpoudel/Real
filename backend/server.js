e3ce1bd1027bc4d755794912dac937ac5ac90178
Bibash Poudel <pdlbibash77@gmail.com>
2022-05-23 15:05:37 +0545

    Connecting to mongoo db database and running server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
+import mongoose from "mongoose";
 
 const app = express();
+
+// connecting to mongoo database
+mongoose
+  .connect(process.env.MONGODB_URL || "mongodb://localhost/realstate", {
+    useNewUrlParser: true, //to get ride from duplicate waring
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connection successfull");
+  })
+  .catch((err) => console.log(err));
 
 app.get(
   "/",
